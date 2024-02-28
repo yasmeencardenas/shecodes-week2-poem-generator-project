497c74c572e246199eedf165acdd2929ca0cb5ff
yasmeencardenas <123997412+yasmeencardenas@users.noreply.github.com>
2024-02-28 22:14:43 +0200

    Added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
+function displayPoem(response) {
+  console.log("poem generated");
+}
+
 function generatePoem(event) {
   event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "6f40cd590a19batc9fb86oe33e342ac5";
+  let context =
+    "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning.";
+  let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayPoem);
 
   new Typewriter("#poem", {
     strings: "La tombe dit à la rose",
