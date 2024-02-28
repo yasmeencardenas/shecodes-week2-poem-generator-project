bf47372f379baf4381df6d7b3d92a7dbac1710d3
yasmeencardenas <123997412+yasmeencardenas@users.noreply.github.com>
2024-02-29 00:18:47 +0200

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ function generatePoem(event) {
   let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Generating poem");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">Generating a French poem about ${instructionsInput.value}</div>`;
 
   axios.get(apiUrl).then(displayPoem);
 
